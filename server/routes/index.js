24c5fc0dd46f8cc831e266f305c48bc69d2be940
Ali Maher <ali.maher.mohammed@gmail.com>
2020-09-27 06:52:12 +0200

    changed the idiot root route response

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const { ensureLoggedin } = require("../middleware/authMiddleware");
 // @desc    Root reoute
 // @route   GET /
 Router.get("/", ensureLoggedin, (req, res) => {
-  res.json("Are you trying To hack me ;(");
+  res.json("You need to log in!");
 });
 
 module.exports = Router;
